076c8a0398ed4c124eedfc815467795f0ca1e88f
FINVIET\quang.tran <quang.tran@finviet.com.vn>
2026-04-01 17:30:41 +0700

    feat(linting): add prettier, lint stage

---
+++ commitlint.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('@commitlint/types').UserConfig} */
+const config = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore', 'revert']
+    ],
+    'subject-case': [2, 'never', ['upper-case', 'pascal-case', 'start-case']],
+    'header-max-length': [2, 'always', 100]
+  }
+}
+
+export default config
